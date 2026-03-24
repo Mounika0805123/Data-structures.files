c799ceaa66de6606a29c67da4c9dc775bbe305af
Mounika0805123 <medamounika55@gmail.com>
2026-03-24 19:47:56 +0530

    Add files via upload

---
+++ QUICKSORT.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+int partition (int a[],int low,int high);
+//swap code
+void swap(int *a,int*b)
+{
+	int temp=*a;
+	*a=*b;
+	*b=temp;
+}
+//quick sort
+void quicksort(int a[],int low,int high)
+{
+	if(low<high)
+	{
+		int location=partition(a,low,high);
+		quicksort(a,low,location-1);
+		quicksort(a,location+1,high);
+	}
+}
+//partition code
+int partition (int a[],int low,int high)
+{
+	int pivot=a[low];
+	int start=low;
+	int end=high;
+	while(start<end)
+	{
+		while(a[start]<=pivot)
+		{start++;
+		}
+		while(a[end]>pivot)
+		{
+			end--;
+	}
+	if(start<end)
+	{
+		swap(&a[start],&a[end]);
+	}
+	}
+	swap(&a[low],&a[end]);
+	return end;
+}
+int main()
+{
+	int n,i;
+	printf("\nenter n size:");
+	scanf("%d",&n);
+	int a[n];
+	printf("\nenter %d elements",n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	quicksort(a,0,n-1);
+	printf("\nafter sorting:");
+	for(i=0;i<n;i++)
+	{
+		printf("\n%d",a[i]);
+	}
+	return 0;
+}
