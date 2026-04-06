0e7df54eaade9eb8879be9398487e054c06fff37
Mounika0805123 <medamounika55@gmail.com>
2026-04-06 23:54:31 +0530

    Add files via upload

---
+++ Deletion of node at front in S.L.L.c
@@ -0,0 +1,53 @@
+//Deletion of node at front in S.L.L
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+	int data;
+	struct node *link;
+};
+struct node *head=0, *temp=0;
+void create()
+{
+	struct node *newnode=NULL;
+	int n,i;
+	printf("\nEnter no. of nodes:");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		newnode=(struct node*)malloc(sizeof(struct node));
+		printf("\nEnter node %d data:",i+1);
+		scanf("%d",&newnode->data);
+		newnode->link=NULL;
+		if(head==NULL)
+		{
+			head=newnode;
+			temp=newnode;
+		}
+		else
+		{
+			temp->link=newnode;
+			temp=newnode;
+		}
+	}
+}
+void display()
+{
+	temp=head;
+	while(temp!=NULL)
+	{
+		printf("\n%d",temp->data);
+		temp=temp->link;
+	}
+}
+int main()
+{
+	create();
+	display();
+	temp=head;
+	head=head->link;
+	free(temp);
+	printf("\nAfter deleting:");
+	display();
+	return 0;
+}
