5840e580b45abc0e61ffd6013f42fcade2e56900
Mounika0805123 <medamounika55@gmail.com>
2026-05-12 07:17:09 +0530

    Add files via upload

---
+++ typedef for structures.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+typedef struct student 
+{
+	int roll;
+	float marks;
+} stu;
+int main()
+{
+	for(int i=1;i<=11;i++)
+	{
+		printf("enter student roll no.");
+		scanf("%d",&a[i].roll);
+		printf("enter student marks:");
+		scanf("%f",&a[i].marks);
+	}
+	
+	return 0;
+}
