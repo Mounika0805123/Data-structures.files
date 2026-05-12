b68fcc753b0ef80973e7ce81f295486ffed462f4
Mounika0805123 <medamounika55@gmail.com>
2026-05-12 07:19:25 +0530

    Add files via upload

---
+++ To print Distance.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int l,b,a;
+	printf("Enter length of a rectangle:");
+	scanf("%d",&l);
+	printf("Enter Breadth of a rectangle:");
+	scanf("%d",&b);
+	a=l*b;
+	printf("\n Area of a Rectangle is:%d",a);
+	return 0;
+}
